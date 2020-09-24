0e9aad997415e7b9ce9a45f8f77744ad37905310
Q. Soerokarso <quincysoeroakrso@novi-education.nl>
2020-09-24 18:43:10 +0200

    Sout toegevoegd

--- src/nl/novi/javaprogrammeren/Main.java
+++ src/nl/novi/javaprogrammeren/Main.java
@@ -3,6 +3,8 @@ package nl.novi.javaprogrammeren;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello!");
+        System.out.println("Altijd alt f4");
     }
 }
